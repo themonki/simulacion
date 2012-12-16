1ee3c1049b30ab4e8da839e3fba52f8199bc7769
felipexxx <felipex1991@gmail.com>
2012-12-16 01:23:00 -0500

    moonte carlo

--- monte carlo/Fermat_extendido.java
+++ monte carlo/Fermat_extendido.java
@@ -10,16 +10,17 @@ public class Fermat_extendido {
 		generador= new Generador(12345);
 	}
 	
-	
+	//http://www.slideshare.net/fivefingers/potencias-modulares
 	
 	 
 	public boolean  MillerRabinPrimalityTest(int certain, long number) {
 	 
-		if (number>2 && ispar(number)) return false;
+		if (number>2 && ispar(number)) return false;// por que el 2 es par y es primo 
 		
 		double s=0.0;
-		double t= number-1;
+		long t= number-1;
 		
+		//System.out.println("t::"+t +"  s::"+s);
 		while (ispar(t)){
 			s++;
 			t=t/2;
@@ -28,21 +29,59 @@ public class Fermat_extendido {
 		
 		System.out.println("t::"+t +"  s::"+s);
 		
-		//for (int i=0;i<10;i++){
-		double x = generador.aleatorio(number);
+		// log_2(1/e)= ln(1/e)/ln 2
+		double e=0.0001;		
+		double k = Math.log(1/e) / Math.log(2);
 		
-		if (Math.pow(x,t)%number==1) return true;
+		k= Math.ceil(k /2);
+		
+		//System.out.println("k::"+k);
+		
+		//System.out.println("t bits::"+Long.toBinaryString(t));
+		
+		
+		
+		
+		
+		//for (int j=0;j<k;j++) {
+		
+		long x = generador.aleatorio(number);
+		
+		long modulo = calcularPotenciaModular(x,t,number);
+		System.out.println("modulo:: "+modulo+ "number"+ number+ " a:: "+x);
+		
+		
+		if (modulo==1) {
+			
+			//System.out.println(x + "::::true");
+			return true;}
 		else {
-			for (int i=0;i<s;i++){
+			
+			
+			for (int i=0;i<=s;i++){
+				int  potencia = (int) Math.pow(2,i);
 				
-				if(Math.pow(x,Math.pow(2,i)*t)%number == number-1   ) return true;
+				long modulo2 = calcularPotenciaModular(x,(long ) (potencia*t),number);
+				
+				System.out.println("modulo2:: "+modulo2+ "number"+ number+ " a:: "+x+"s"+s);
+				
+				if(modulo2 == number-1   ){ 
+					//System.out.println(x + "true");
+					
+					return true;
+					}
 				
 			}
 			
 			
 		}
 				
-		System.out.println(x);
+		//System.out.println(x + " a  false");
+		
+		return false ;
+		
+		
+		
 		//}
 		
 		
@@ -50,11 +89,8 @@ public class Fermat_extendido {
 		
 		
 		
-		
-		
-		
 		/* Factor n  1 as 2s t where t is odd. */
-		return false;
+		
 		
 		
 	   /* BigInteger n = new BigInteger("");
@@ -66,9 +102,28 @@ public class Fermat_extendido {
 
 
 
+	public  long calcularPotenciaModular(long base, long  exponente, long modulo){
+	       
+        long resultado = 1;
+        for(int i = 1; i<=exponente; i++){
+            resultado = (resultado*base)%modulo;
+        
+        } 
+        
+        return resultado;
+    }
+	
+	
+
 	private boolean ispar(double t) {
 		// TODO Auto-generated method stub
-		if (t%2==0) return true;
+		if (t%2==0) {
+			
+			//System.out.println("es par ::"+t);
+			return true;}
+		
+		
+		//System.out.println("NO es par ::"+t);
 		return false;
 	}
 	
@@ -79,8 +134,8 @@ public class Fermat_extendido {
 		
 		Integer [] a= { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89 , 97};
 		
-		for (int i=0;i<10;i++)
-		System.out.println(ferma.MillerRabinPrimalityTest(0, a[i]));
+		//for (int i=0;i<10;i++)
+		System.out.println(ferma.MillerRabinPrimalityTest(0, 1001));
 		
 		
 		
